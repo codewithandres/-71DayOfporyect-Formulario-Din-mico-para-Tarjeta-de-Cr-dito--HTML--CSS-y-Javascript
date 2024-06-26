eb0764076a21361caed6c025cd24962fcb147652
Dize dev <axiozweb2022@gmail.com>
2024-06-25 23:56:14 -0500

    se agrego la la carga del mes y el año de expiracion dinamicamente con javascript

--- js/app.js
+++ js/app.js
@@ -8,3 +8,18 @@ btnAbrirFormulario.addEventListener('click', () => {
 	btnAbrirFormulario.classList.toggle('active'),
 		formulario.classList.toggle('active');
 });
+
+for (let i = 0; i <= 12; i++) {
+	let option = document.createElement('option');
+	option.value = i;
+	option.innerHTML = i;
+	formulario.selectMes.appendChild(option);
+}
+
+const yearActuala = new Date().getFullYear();
+for (let i = yearActuala; i <= yearActuala + 8; i++) {
+	let option = document.createElement('option');
+	option.value = i;
+	option.innerHTML = i;
+	formulario.selectYear.appendChild(option);
+}
