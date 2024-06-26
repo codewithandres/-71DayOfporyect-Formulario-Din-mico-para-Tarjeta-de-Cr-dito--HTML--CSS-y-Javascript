61a870f3d4169a8855f78099387ebf2b5d00fbca
Dize dev <axiozweb2022@gmail.com>
2024-06-25 23:34:00 -0500

    se agregaron estilos del formulario y las animacionees con javascript

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
-const tarjeta = document.querySelector('.contenedor');
+const tarjeta = document.querySelector('#tarjeta');
 const btnAbrirFormulario = document.querySelector('#btn-abrir-formulario');
+const formulario = document.querySelector('#formulario-tarjeta');
 
 tarjeta.addEventListener('click', () => tarjeta.classList.toggle('active'));
 
-btnAbrirFormulario.addEventListener('click', () =>
+btnAbrirFormulario.addEventListener('click', () => {
 	btnAbrirFormulario.classList.toggle('active'),
-);
+		formulario.classList.toggle('active');
+});
