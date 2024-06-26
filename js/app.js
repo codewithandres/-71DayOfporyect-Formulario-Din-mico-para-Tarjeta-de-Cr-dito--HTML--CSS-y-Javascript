d79ab26a8383d749900b41dd6e0fb3940e420bd3
Dize dev <axiozweb2022@gmail.com>
2024-06-25 22:01:15 -0500

    diseño del boton para abrir el formulario

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 const tarjeta = document.querySelector('.contenedor');
+const btnAbrirFormulario = document.querySelector('#btn-abrir-formulario');
 
-tarjeta.addEventListener('click', () => {
-	tarjeta.querySelector('#tarjeta').classList.toggle('active');
-});
+tarjeta.addEventListener('click', () => tarjeta.classList.toggle('active'));
+
+btnAbrirFormulario.addEventListener('click', () =>
+	btnAbrirFormulario.classList.toggle('active'),
+);
