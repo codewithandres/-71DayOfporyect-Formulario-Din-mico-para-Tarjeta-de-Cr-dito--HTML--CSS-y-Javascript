17203997546d7d92c8f1237affc6dfee57603811
Dize dev <axiozweb2022@gmail.com>
2024-06-27 10:25:30 -0500

    se optimizo un poco el codigo usando la destructuracion

--- js/app.js
+++ js/app.js
@@ -1,25 +1,40 @@
-const tarjeta = document.querySelector('#tarjeta');
-const btnAbrirFormulario = document.querySelector('#btn-abrir-formulario');
-const formulario = document.querySelector('#formulario-tarjeta');
+const formularioElement = {
+	tarjeta: document.querySelector('#tarjeta'),
+	btnAbrirFormulario: document.querySelector('#btn-abrir-formulario'),
+	formulario: document.querySelector('#formulario-tarjeta'),
+};
 
 tarjeta.addEventListener('click', () => tarjeta.classList.toggle('active'));
 
-btnAbrirFormulario.addEventListener('click', () => {
+formularioElement.btnAbrirFormulario.addEventListener('click', () => {
+	const { btnAbrirFormulario, formulario } = formularioElement;
+
 	btnAbrirFormulario.classList.toggle('active'),
 		formulario.classList.toggle('active');
 });
 
 for (let i = 0; i <= 12; i++) {
 	let option = document.createElement('option');
+
 	option.value = i;
 	option.innerHTML = i;
-	formulario.selectMes.appendChild(option);
+	formularioElement.formulario.selectMes.appendChild(option);
 }
 
 const yearActuala = new Date().getFullYear();
 for (let i = yearActuala; i <= yearActuala + 8; i++) {
+	const { formulario } = formularioElement;
 	let option = document.createElement('option');
+
 	option.value = i;
 	option.innerHTML = i;
 	formulario.selectYear.appendChild(option);
 }
+
+formularioElement.formulario.inputNumero.addEventListener('input', (event) => {
+	const { formulario } = formularioElement;
+	let valorInput = event.target.value;
+
+	formulario.inputNumero.value = valorInput;
+	console.log(valorInput);
+});
